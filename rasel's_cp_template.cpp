accd2fe7018b48e41a25a78f84c248d31e2fbeb8
Shamim Ahammad Rasel <87210321+rasel-33@users.noreply.github.com>
2023-10-17 07:04:47 +0600

    Debug section added

--- rasel's_cp_template.cpp
+++ rasel's_cp_template.cpp
@@ -1,71 +1,84 @@
 #include<bits/stdc++.h>
-
-#define EPS          1E-9
-#define PI           acos(-1)
-#define SQR(n)       (n*n)
 using namespace std;
-#define M            998244353
-#define INF          (1<<30)-1+(1<<30)
-#define fast         ios::sync_with_stdio();cin.tie(nullptr)
-#define all(v)       (v).begin(), (v).end()
+
+#ifdef RASEL
+#include "debug.h"
+#else
+#define dbg(...);
+#define debug(...);
+#define crndl;
+#endif
+
+
+#define MOD          1000000007
+#define MOD1         998244353
+#define MOD2         972663749
+#define EPS          1e-12
 #define ll           long long
-#define f0(i, n)     for(int (i) = 0; i <  (n); i++)
-#define f1(i, n)     for(int (i) = 1; i <= (n); i++)
-#define endl         "\n"
+#define pll          pair<ll,ll>
 #define pb           push_back
 #define UNIQUE(v)    (v).erase(unique(all(v)), (v).end())
-#define sz()         size()
-#define mxe(a,n)     (*max_element(a,a+n))
-#define mne(a,n)     (*min_element(a,a+n))
+#define ff           first
+#define ss           second
+#define sz(x)        ((int)(x).size())
+#define all(x)       (x).begin(), (x).end()
 #define countbit(x)  __builtin_popcount(x)
+
 //moves
 int kdx[8]={ 2, 1, -1, -2, -2, -1, 1, 2 };//knights move
 int kdy[8]={ 1, 2, 2, 1, -1, -2, -2, -1 };//knights move
 int dx[] = { -1,+0,+1,+0,-1,-1,+1,+1};
 int dy[] = { +0,-1,+0,+1,+1,-1,+1,-1};
-//bit
-int     Set(int N,int pos){ return N=N | (1<<pos);}
-int     Reset(int N,int pos){ return N= N & ~(1<<pos);}
-bool    Check(int N,int pos){ return (bool)(N & (1<<pos));}
-template< class T, class X > inline T   togglebit(T a, X i) { T t=1;return (a^(t<<i)); }
-//fn
-template <typename T> T     BigMod (T b,T p,T m){if (p == 0) return 1;if (p%2 == 0){T s = BigMod(b,p/2,m);return ((s%m)*(s%m))%m;}return ((b%m)*(BigMod(b,p-1,m)%m))%m;}
-template <typename T> T     ModInv (T b,T m){return BigMod(b,m-2,m);}
-template <typename T> T     POW(T B,T P){ if(P==0) return 1; if(P&1) return B*POW(B,P-1);  else return SQR(POW(B,P/2));}
-template <typename T> T     Dis(T x1,T y1,T x2, T y2){return sqrt( SQR(x1-x2) + SQR(y1-y2) );}
-template <typename T> T     Angle(T x1,T y1,T x2, T y2){ return atan( double(y1-y2) / double(x1-x2));}
-template <typename T> void  yes(T a){if(a==1)printf("YES\n");else printf("Yes\n");}
-template <typename T> void  no(T a){if(a==1)printf("NO\n");else printf("No\n");}
-//file input
-void xxx(){
-    #ifdef LOCAL 
-    freopen("input.txt", "r", stdin);freopen("output.txt", "w",stdout);
-    #endif
-}
+
+template <typename T> void  yes(T a){if(a==1)printf("Yes\n");else printf("YES\n");}
+template <typename T> void  no(T a){if(a==1)printf("No\n");else printf("NO\n");}
+template <typename T> T     min (T a,T b){return min(a, b);}
+template <typename T> T     max (T a,T b){return max(a, b);}
+
+ll expo(ll a, ll b, ll m){if(b == 0) return 1ll;ll ret = expo(a, b/2, m);ret = (ret%m * ret%m)%m;if(b%2) ret = (ret*a)%m;return ret;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;} 
+
+ll n, m, k, x, y, h, a, b, c, d, e, f,q, r, w, p, g, X1, X2, Y1, Y2, xdiff, ydiff,z, i, j;
+string s, sa, sb, str;
 
 
-string str, sa, sb;
-ll n, m, k, x, y, h, w, a,b,c;
-ll arr[1000006];
+ll ar[200005], br[100005], cr[100005];
+ll gg[1001][1001];
 
 
 
-void solve(int tc){
-    scanf("%lld", &n);
-    printf("%lld\n",n);
-    
+void solve(){
+    bool ok = true, can = false;
+    ll  sum = 0, mx = 0, mn = 1e18, tot=0, len, idx;
+
+
 }
 
 
 
 int main(){
-    xxx();
+#ifdef RASEL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w",stdout);
+    printf("------------------------------------\n"); time_t start = clock();
+#endif
+    // cout << fixed << setprecision(10);
+
     int tc = 1, cas_no = 0;
-    scanf("%d", &tc);
-    while(tc--){cas_no++;
-        solve(cas_no);
+    // scanf("%d", &tc);
+    while(tc--){++cas_no;
+    // printf("Case %d: ",cas_no);
+    solve();
     }
-    return 0;
+
+#ifdef RASEL
+    printf("\n--------Executed Succesfully--------\n");
+    fprintf(stderr, "Elapsed time: %lld ms    ", (ll)(1000.0 * (double)(clock() - start) / CLOCKS_PER_SEC));
+    printf("\nCheck constraints and delcared size\n");
+#endif
+return 0;
 }
-
-
